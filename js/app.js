525058671f0e45b8f13428898d9d11a19b9856c7
Paul Pichon F <paul10_barca@hotmail.com>
2023-03-14 21:31:51 -0600

    Actualizacion

--- js/app.js
+++ js/app.js
@@ -1,79 +1,76 @@
 //variables
-//variable para resultados
-const resultado = document.querySelector('#resultado');
-//formulario
 const formulario = document.querySelector('#formulario');
+//variable respuesta
+const resultado = document.querySelector('#resultado');
 
-//similar a DOMContentLoaded CON listener -> window.onload
+//listener
 window.onload = () => {
-    //agregar un listener al formulario
+    
+    //listener al formulario
     formulario.addEventListener('submit', validarFormulario);
 
-
-
 }
+
+//funciones
 //funcion para validar el formulario
 function validarFormulario( e ) {
     //prevenir la accion por defecto
     e.preventDefault();
-    //valor del input de busqueda
-    const terminoBusqueda = document.querySelector('#termino').value;    
-    //verificar con un if si no viene vacio
-    if (terminoBusqueda === '') {
-        //funcion para mostrar mensaje de alerta
-        mostrarAlerta('Agrega un termino de busqueda');
-        //retornamos para que ya no siga ejecutando codigo
+    //obtener el value del input
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    //validar si terminoBusqueda esta vacio
+    if ( terminoBusqueda === '') {
+        //llamar funcion para mostrar un mensaje de error
+        mostrarMesaje("El input no puede estar vacio");
+        //return para que ya no siga ejecutando codigo
         return;
     }
-
-    //funcion para buscar las imagenes y le pasamos como argumento el termino de la busqueda
+    //llamar la funcion para hacer la busqueda con la API
     buscarImagenes( terminoBusqueda );
-
-
-}
-//funcion para mostrar alerta
-function mostrarAlerta( mensaje ) {
-
-    //verificar que no haya otra alerta previa
-    const existeAlerta = document.querySelector('.bg-red-100');
-    //if 
-    if (!existeAlerta) {
-        ///html del mensaje a mostrar
-        const alerta = document.createElement('P');
-        //estilos
-        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
-        //innerhtml
-        alerta.innerHTML = `
-            <strong class="font-bold">¡Error!</strong>
-            <span class="block sm:inline">${mensaje}</span>
-        `;
-        //renderizar
-        resultado.appendChild( alerta );
-
-        //quitar alerta despues de 3 segundos
-        setTimeout(() => {
-            //quitar alerta
-            alerta.remove();
-        }, 3000);
-    }
-
     
 }
 
-//funcion para buscar el termino de la buscada
-function buscarImagenes( termino ) {
-    //llave de la API
-    const key = '30907566-3221c4d1c76ae349991022cae';
-    //url de los endpoints
-    const url = `https://pixabay.com/api/?key=${ key }&q=${ termino }&image_type=photo`;
+//funcion para mostrar mensaje en la pantalla
+function mostrarMesaje( mensaje ) {
 
-    fetch( url )
-        .then( respuesta => respuesta.json() )
-        .then( resultado => mostrarImagenes( resultado.hits ) )
+    //verificar si hay una alerta previa
+    const existeAlerta = document.querySelector('.alerta');
+    //si existe la alerta no se muestra otra vez
+    if ( !existeAlerta ) {//true o false
+        //construir el html
+        const alerta = document.createElement('P');
+        //estilos
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'alerta');
+        //innerhtml
+        alerta.innerHTML = `
+            <strong class='font-bold''>¡ERROR!</strong>
+            <span class='block sm:inline'>${ mensaje }</span>
+        `;
+
+        //renderizar el mensaje
+        resultado.appendChild( alerta );
+        //quitar el mensaje despues de 3 segundos
+        setTimeout(() => {
+            //quitar alerta
+            alerta.remove();
+        }, 3000);    
+    }
 
 }
+//funcion para crear la busqueda de imagenes con la API PIXABAY
+function buscarImagenes( termino ) {
+    //key
+    const key = '30907566-3221c4d1c76ae349991022cae';
+    //url 
+    const url = `https://pixabay.com/api/?key=${ key }&q=${ termino }&image_type=photo`;
+    //fetch
+    fetch( url )
+        .then( respuesta => respuesta.json() )
+            .then( resultado => mostrarImagenes( resultado.hits ) )
 
-//funcion para mostrar las imagenes
+}
+//funcion para mostrar las imagenes en pantalla
 function mostrarImagenes( imagenes ) {
-
+    console.log(imagenes, "imagenes");
 }
